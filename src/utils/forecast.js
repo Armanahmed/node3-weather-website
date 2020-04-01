beff9794ff6ddcdeeff5a5fc9072fbb8404c06b4
Armanahmed <aaarmanapu@gmail.com>
2020-04-02 00:18:29 +0600

    Initial Commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,16 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/796cf1ba8a54a418c256c0b97ef8f972/'+ latitude + ',' + longitude;
+    request({url, json: true}, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to weather service', undefined);
+        } else if (body.error) {
+            callback('Unable to find location', undefined);
+        } else {
+            callback(error, body.daily.data[0].summary + " It is currently " + body.currently.temperature + " degrees out. There is a " + body.currently.precipProbability + "% chance of rain.");
+        }
+    });
+};
+
+module.exports = forecast;
